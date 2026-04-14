8841bb47f9f0bb236c7842168c2e5b050b7f9a6d
EriDiva <divataralandu@gmail.com>
2026-04-14 21:50:41 +0700

    Delete src directory

--- src/data/categories.js
+++
@@ -1,8 +0,0 @@
-export const CategoryList = [
-  { id: 1, name: "All" },
-  { id: 2, name: "Fantasy" },
-  { id: 3, name: "Finance" },
-  { id: 4, name: "Romance" },
-  { id: 5, name: "History" },
-  { id: 6, name: "Psychology" },
-];
